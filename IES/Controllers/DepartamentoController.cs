8f3518ade9d75011ca7c512d0a6b1c68f9571735
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-29 12:20:38 -0400

    Criadas actions e views Create(GET e POST) e Index.

--- IES/Controllers/DepartamentoController.cs
+++ IES/Controllers/DepartamentoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using IES.Data;
+using IES.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,6 +21,33 @@ namespace IES.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.Departamentos.OrderBy(c => c.Nome).ToListAsync());
+        }
+
+        // GET: Departamento/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nome")] Departamento departamento)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(departamento);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Não foi possível inserir os dados.");
+            }
+
+            return View(departamento);
         }
     }
 }
