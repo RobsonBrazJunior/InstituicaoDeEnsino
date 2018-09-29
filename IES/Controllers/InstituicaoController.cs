e93d87fb3d1b65fec8728ba2b317b87bb1bf1fa1
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-29 02:06:28 -0400

    Criada nova action para receber os dados para o modelo e processar o registro.

--- IES/Controllers/InstituicaoController.cs
+++ IES/Controllers/InstituicaoController.cs
@@ -56,5 +56,14 @@ namespace IES.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Instituicao instituicao)
+        {
+            instituicoes.Add(instituicao);
+            instituicao.InstituicaoID = instituicoes.Select(i => i.InstituicaoID).Max() + 1;
+            return RedirectToAction("Index");
+        }
     }
 }
