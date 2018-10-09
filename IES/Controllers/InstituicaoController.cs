ec0197792d8824a779cb19e139e5f2f91384057f
Junior Braz <robsonbrazjunior@gmail.com>
2018-10-09 14:50:27 -0400

    Criada classe Controller InstitituicaoController

---
+++ IES/Controllers/InstituicaoController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IES.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
