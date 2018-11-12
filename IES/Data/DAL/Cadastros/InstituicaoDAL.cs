188496e03216f23182cd5320519aa3e7d62e5af1
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2018-11-12 11:14:32 -0300

    Separando camada de persistência.

---
+++ IES/Data/DAL/Cadastros/InstituicaoDAL.cs
@@ -0,0 +1,20 @@
+﻿using Modelo.Cadastros;
+using System.Linq;
+
+namespace IES.Data.DAL.Cadastros
+{
+    public class InstituicaoDAL
+    {
+        private IESContext _context;
+
+        public InstituicaoDAL(IESContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Instituicao> ObterInstituicoesClassificadasPorNome()
+        {
+            return _context.Instituicoes.OrderBy(b => b.Nome);
+        }
+    }
+}
--- IES/Data/IESContext.cs
+++ IES/Data/IESContext.cs
@@ -9,11 +9,5 @@ namespace IES.Data
 
         public DbSet<Departamento> Departamentos { get; set; }
         public DbSet<Instituicao> Instituicoes { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Departamento>().ToTable("Departamento");
-        }
     }
 }
