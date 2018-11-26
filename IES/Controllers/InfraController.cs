158ec2e0bc6f4d6b3772d9324dfd7477ad86c280
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2018-11-26 09:14:42 -0300

    Criada action POST de RegistrarNovoUsuario

--- IES/Controllers/InfraController.cs
+++ IES/Controllers/InfraController.cs
@@ -41,5 +41,42 @@ namespace IES.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             return View();
         }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RegistrarNovoUsuario(RegistrarNovoUsuarioViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var user = new UsuarioDaAplicacao { UserName = model.Email, Email = model.Email};
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("Usuário criou uma nova conta com senha.");
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    _logger.LogInformation("Usuário acesso com a conta criada.");
+                    return RedirectToLocal(returnUrl);
+                }
+                AddErros(result);
+            }
+            return View(model);
+        }
+
+        private void AddErros (IdentityResult result)
+        {
+            foreach(var error in result.Errors)
+                ModelState.AddModelError(string.Empty, error.Description);
+        }
+
+        private IActionResult RedirectToLocal (string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+                return Redirect(returnUrl);
+            else
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
     }
 }
