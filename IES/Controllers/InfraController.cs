e61d8d6701fe2b023aa80244c3b934bc7e0aefb8
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2018-11-26 10:34:11 -0300

    Implementado sessão de Logout.

--- IES/Controllers/InfraController.cs
+++ IES/Controllers/InfraController.cs
@@ -34,6 +34,25 @@ namespace IES.Controllers
             return View();
         }
 
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Acessar(AcessarViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Senha, model.LembrarDeMim, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("Usuário Autenticado.");
+                    return RedirectToLocal(returnUrl);
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Falha na tentativa de login.");
+            return View(model);
+        }
+
         [HttpGet]
         [AllowAnonymous]
         public IActionResult RegistrarNovoUsuario(string returnUrl = null)
@@ -63,6 +82,14 @@ namespace IES.Controllers
                 AddErros(result);
             }
             return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Sair()
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("Uusário realizou logout.");
+            return RedirectToAction(nameof(HomeController.Index), "Home");
         }
 
         private void AddErros (IdentityResult result)
