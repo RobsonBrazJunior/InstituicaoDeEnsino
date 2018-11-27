a005855e3577ab628fa26bf1756ad2b25212f022
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2018-11-27 11:09:25 -0300

    Criada classe ProfessorController

---
+++ IES/Areas/Docente/Controllers/ProfessorController.cs
@@ -0,0 +1,25 @@
+﻿using IES.Data;
+using IES.Data.DAL.Cadastros;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IES.Areas.Docente.Controllers
+{
+    [Area("Docente")]
+    public class ProfessorController : Controller
+    {
+        public readonly IESContext _context;
+        public readonly InstituicaoDAL instituicaoDAL;
+        public readonly DepartamentoDAL departamentoDAL;
+        public readonly CursoDAL cursoDAL;
+        public readonly ProfessorDAL professorDAL;
+
+        public ProfessorController (IESContext context)
+        {
+            _context = context;
+            instituicaoDAL = new InstituicaoDAL(context);
+            departamentoDAL = new DepartamentoDAL(context);
+            cursoDAL = new CursoDAL(context);
+            professorDAL = new ProfessorDAL(context);
+        }
+    }
+}
