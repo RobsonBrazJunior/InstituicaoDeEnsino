b4ff9e61b7dd0fc7e135655942adfa9eab31cbec
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2018-11-27 14:07:09 -0300

    Criada classe ProfessorDAL

---
+++ IES/Data/DAL/Docente/ProfessorDAL.cs
@@ -0,0 +1,20 @@
+﻿using Modelo.Docente;
+using System.Linq;
+
+namespace IES.Data.DAL.Docente
+{
+    public class ProfessorDAL
+    {
+        private IESContext _context;
+
+        public ProfessorDAL(IESContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Professor> ObterProfessoresClassificadosPorNome()
+        {
+            return _context.Professores.OrderBy(b => b.Nome);
+        }
+    }
+}
